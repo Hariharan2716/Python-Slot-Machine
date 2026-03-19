933c6c5f543e6c61fe419910bf5d630c368107dd
Hariharan <hariharan.m2716@gmail.com>
2026-03-19 23:56:23 +0530

    Added a spin function to generate the ouput of generating slots

--- main.py
+++ main.py
@@ -1,9 +1,48 @@
 # Pyhton has no constant rule like JavaScript so we use naming rules of PEP 8
 # Capital Snake case is rule to use for constant variables
+import random #This imports module (random) that generates number between 0s and 1s
 
 MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+symbol_value = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2
+}
+
+def get_slot_machine_spin(rows, cols, symbols):
+  all_symbols = []
+  for symbol, symbol_count in symbols.items():
+      for _ in range(symbol_count):
+          all_symbols.append(symbol)
+
+  columns = []
+  for _ in range(cols):
+      column = []
+      current_symbols = all_symbols[:] # this is to copy? because if there is a change in the orginal array it would effect
+      for _ in range(rows):
+          value = random.choice(current_symbols)
+          current_symbols.remove(value)
+          column.append(value)
+
+      columns.append(column)
+
+  return columns
+
+# To print the slot machine to get a view
 
 def deposit():
   while True:
